5e6945f0900ad218f5076b75bfd809800aa215a8
hao.tan <hao.tan@paiyue.com>
2018-08-30 18:45:32 +0800

    update

--- pages/index/index.js
+++ pages/index/index.js
@@ -10,21 +10,19 @@ Page({
         //搜索条件
         keyword:'',
         //商区 商圈码
-        code:null,
+        code:{},
         // code: {"level1": "100", "level2": "101", "level3": "10101"},
         // code: { "level1": "100", "level2": "101", "level3": "" },
         // code: { "level1": "100", "level2": "", "level3": "" },
         //智能排序
         lon_lat:{},
         // lon_lat: {"longitude": "116.366225", "latitude":"39.935242"},
-        // orderType:'',
-        orderType: "familiarity",    //熟悉度
-        // orderType: "distance",      //距离
-        // orderType: "time",          //时间
+        orderType:'',
+        orderTypeEnums: ["familiarity", "familiarity", "distance", "time"], //默认 熟悉度  距离  时间
         //打球时间
-        // date: '',
         date:'',  //日期
         time:'',  //时间
+        timeType:["morning","afternoon","night"],
         motto: 'Hello World',
         userInfo: {},
         hasUserInfo: false,
@@ -54,32 +52,9 @@ Page({
         shownavindex: '',
         games: ''
     },
-    /**
-     * 
-    showInput: function () {
-        this.setData({
-            inputShowed: true
-        });
-    },
-    hideInput: function () {
-        this.setData({
-            inputVal: "",
-            inputShowed: false
-        });
-    },
-    clearInput: function () {
-        this.setData({
-            inputVal: ""
-        });
-    },
-     * 
-     */
     inputTyping: function (e) {
         let keyword = e.detail.value;
         this.setData({keyword:keyword})
-        // this.setData({
-        //     inputVal: e.detail.value
-        // });
     },
     onLoad: function (options) {
         wx.setNavigationBarTitle({
@@ -198,7 +173,7 @@ Page({
     bindDateChange: function (e) {//时间选择
         console.log('picker发送选择改变，携带值为', e.detail.value)
         this.setData({
-            px_time: e.detail.value
+            date: e.detail.value
         })
     },
     // getUserInfo: function (e) {
@@ -230,20 +205,6 @@ Page({
     //查询球局数据
     queryGame:function(){
       let that = this;
-      let locations = that.data.lon_lat;
-      //获取当前位置经纬度
-      // wx.getLocation({
-      //   type: 'wgs84',
-      //   success: function (res) {
-      //     var latitude = res.latitude;
-      //     var longitude = res.longitude;
-      //     locations.longitude = longitude;
-      //     locations.latitude  = latitude;
-      //     that.setData({
-      //       lon_lat : locations
-      //     })
-      //   }
-      // })
       wx.request({
         url: 'http://localhost:6677/game',
         method: "GET",
@@ -270,6 +231,11 @@ Page({
           }
         }
       })
+      this.setData({
+        code:{},
+        date:'',
+        time:''
+      })
     },
 
     // 查看球场地址详情
@@ -287,5 +253,51 @@ Page({
         wx.navigateTo({
             url: '../activity/apply/apply?game=' + JSON.stringify(game),
         })
-    }
+    },
+
+    // 智能排序
+    intelligentSort:function(event){
+      this.hidebg();
+      let idx = event.currentTarget.dataset.idx;
+      if(idx == 2){
+        this.getLoc();
+      }
+      this.setData({
+        orderType: this.data.orderTypeEnums[idx]
+      })
+      this.queryGame();
+      this.setData({
+        orderType:'',
+        lon_lat:{}
+      })
+    },
+    //获取当前定位
+    getLoc:function(){
+      let that =this;
+      let locations = that.data.lon_lat;
+      //获取当前位置经纬度
+      wx.getLocation({
+        type: 'wgs84',
+        success: function (res) {
+          var latitude = res.latitude;
+          var longitude = res.longitude;
+          locations.longitude = longitude;
+          locations.latitude  = latitude;
+          that.setData({
+            lon_lat : locations
+          })
+        }
+      })
+    },
+    //选择时间 上/下/晚
+    selectTime:function(event){
+      this.hidebg();
+      let idx = event.currentTarget.dataset.idx;
+      this.setData({time:this.data.timeType[idx]})
+    },
+    
+    
+    
+
+
 })
